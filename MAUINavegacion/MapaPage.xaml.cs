426f7c7fe48bd1f28cd4bcfab2238035f7682739
benjamacmullen <benjamacsk8man@gmail.com>
2026-08-09 19:54:23 -0300

    mapa funcionanado en andorid

--- MAUINavegacion/MapaPage.xaml.cs
+++ MAUINavegacion/MapaPage.xaml.cs
@@ -1,3 +1,6 @@
+using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
+
 namespace MAUINavegacion;
 
 public partial class MapaPage : ContentPage
@@ -5,6 +8,21 @@ public partial class MapaPage : ContentPage
     public MapaPage()
     {
         InitializeComponent();
+
+        MostrarMapa();
+    }
+
+    private void MostrarMapa()
+    {
+        Location puntaDelEste =
+            new Location(
+                -34.9475,
+                -54.9338);
+
+        Mapa.MoveToRegion(
+            MapSpan.FromCenterAndRadius(
+                puntaDelEste,
+                Distance.FromKilometers(5)));
     }
 
     private async void OnVolverClicked(
